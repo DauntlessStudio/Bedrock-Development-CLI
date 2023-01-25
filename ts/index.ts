8b860723c4f7d1e8d5f2df2561cf6c949b7410ca
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2023-01-25 12:10:47 -0600

    v1.0.3

--- ts/index.ts
+++ ts/index.ts
@@ -19,7 +19,7 @@ program
   .on('option:bpath', Global.setBehaviorPath)
   .option('-i, --indent <number>', 'set indent level for JSON files', '4')
   .on('option:indent', Global.setIndentLevel)
-  .version('1.0.2');
+  .version('1.0.3');
 
 // #region New Commands
 let createNew = program.command('new')
