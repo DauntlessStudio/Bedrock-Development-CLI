0217b7cd81eecc8d84a6bb557493fe0366999459
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2023-01-31 17:26:22 -0600

    v1.0.4

--- ts/index.ts
+++ ts/index.ts
@@ -7,8 +7,10 @@ import * as Block from 'bedrock-development/bin/app/block';
 import * as Animation from 'bedrock-development/bin/app/animations';
 import * as Function from 'bedrock-development/bin/app/functions';
 import * as Package from 'bedrock-development/bin/app/package_manager';
+import axios from 'axios';
 
 let program = new Command();
+const version = '1.0.4';
 
 program
   .name('bed')
@@ -19,7 +21,8 @@ program
   .on('option:bpath', Global.setBehaviorPath)
   .option('-i, --indent <number>', 'set indent level for JSON files', '4')
   .on('option:indent', Global.setIndentLevel)
-  .version('1.0.3');
+  .version(version)
+  .action(printVersion)
 
 // #region New Commands
 let createNew = program.command('new')
@@ -31,7 +34,8 @@ createNew.command('entity')
   .option('-l, --lang', 'add lang file')
   .addOption(new Option('-t, --type <char>', 'set entity type: dummy, passive, hostile',).choices(['d', 'h', 'p']).default('d', 'dummy'))
   .option('-c, --client', 'create client entity in the resource path. Will also create a default geo and texture for the entity')
-  .action(triggerCreateNewEntity);
+  .action(triggerCreateNewEntity)
+  .hook('postAction', printVersion);
 
 createNew.command('item')
   .description('Creates new Bedrock items')
@@ -40,7 +44,8 @@ createNew.command('item')
   .option('-e, --edible', 'is this item edible (designed for interacts)')
   .option('-s, --stack <stack_size>', 'max stack size', '64')
   .option('-a, --attachable', 'is this item an attachable')
-  .action(triggerCreateNewItem);
+  .action(triggerCreateNewItem)
+  .hook('postAction', printVersion);
 
 createNew.command('block')
   .description('Creates new Bedrock blocks')
@@ -48,7 +53,8 @@ createNew.command('block')
   .option('-l, --lang', 'add lang file')
   .option('-e, --emissive <emission>', 'block emmission level [0.0-1.0]')
   .option('-t, --table', 'create a loot table')
-  .action(triggerCreateNewBlock);
+  .action(triggerCreateNewBlock)
+  .hook('postAction', printVersion);
 
 createNew.command('anim')
   .description('Creates new Bedrock behavior animations')
@@ -56,7 +62,8 @@ createNew.command('anim')
   .option('-l, --loop', 'should the animation loop')
   .option('-c, --commands <commands>', 'the commands to play', '/say anim_name')
   .option('-t, --time', 'the animation length', '1.0')
-  .action(triggerCreateNewAnimation);
+  .action(triggerCreateNewAnimation)
+  .hook('postAction', printVersion);
 
 createNew.command('ctrl')
   .description('Creates new Bedrock behavior animation controllers')
@@ -66,7 +73,8 @@ createNew.command('ctrl')
   .option('-a, --anim <animations>', 'the animations to play')
   .option('-q, --query [query]', 'the query to transition from default', 'true')
   .addOption(new Option('-t, --transition [transition]', 'the query to transition back to default').preset('true'))
-  .action(triggerCreateNewController);
+  .action(triggerCreateNewController)
+  .hook('postAction', printVersion);
 
 createNew.command('function')
   .description('Creates new Bedrock functions')
@@ -76,13 +84,15 @@ createNew.command('function')
   .option('-d, --description <description>', 'the description of the function to be used as a comment')
   .option('-s, --source <source>', 'where is this function called from, used as a comment')
   .addHelpText('before', 'special characters can be used to provide additional formatting. $(F)f inserts the filename into the command, $(I)i inserts the index of the batch generated command')
-  .action(triggerCreateNewFunction);
+  .action(triggerCreateNewFunction)
+  .hook('postAction', printVersion);
 
 createNew.command('vanilla')
   .description('Imports a vanilla Bedrock entity')
   .argument('<names...>', 'entity files as "player.json"')
   .option('-c, --client', 'create client entity in the resource path. Will also create a default geo and texture for the entity')
-  .action(triggerCreateVanillaEntity);
+  .action(triggerCreateVanillaEntity)
+  .hook('postAction', printVersion);
 // #endregion
 
 // #region Entity Commands
@@ -95,28 +105,32 @@ entity.command('anim')
   .option('-t, --type <family type>', 'filter entities by family type')
   .option('-f, --file <file>', 'the entity files that should be modified', '**/*.json')
   .option('-s, --script', 'should these animations be added to script')
-  .action(triggerEntityAddAnim);
+  .action(triggerEntityAddAnim)
+  .hook('postAction', printVersion);
 
 entity.command('group')
   .description('Adds a component group to entities')
   .argument('<group>', 'the component group as a json object {group_name:{minecraft:is_baby:{}}}')
   .option('-t, --type <family type>', 'filter entities by family type')
   .option('-f, --file <file>', 'the entity files that should be modified', '**/*.json')
-  .action(triggerEntityAddGroup);
+  .action(triggerEntityAddGroup)
+  .hook('postAction', printVersion);
 
 entity.command('component')
   .description('Adds a component to entities')
   .argument('<component>', 'the component as a json object {minecraft:is_baby:{}}')
   .option('-t, --type <family type>', 'filter entities by family type')
   .option('-f, --file <file>', 'the entity files that should be modified', '**/*.json')
-  .action(triggerEntityAddComponent);
+  .action(triggerEntityAddComponent)
+  .hook('postAction', printVersion);
 
 entity.command('sensor')
   .description('Adds a damage sensor to entities')
   .argument('<sensor>', 'the damage sensor as a json object {cause: \\"all\\", deals_damage: false}')
   .option('-t, --type <family type>', 'filter entities by family type')
   .option('-f, --file <file>', 'the entity files that should be modified', '**/*.json')
-  .action(triggerEntityAddDamageSensor);
+  .action(triggerEntityAddDamageSensor)
+  .hook('postAction', printVersion);
 
 let property = entity.command('property')
   .description('Adds property or property events to entities');
@@ -130,7 +144,8 @@ property.command('add')
   .option('-v, --values <values...>', 'the values, either a list of enum values or a min-max')
   .option('-d, --default <default>', 'the default value')
   .option('-c, --client', 'should use client_sync')
-  .action(triggerEntityAddProperty);
+  .action(triggerEntityAddProperty)
+  .hook('postAction', printVersion);
 
 property.command('event')
   .description('Adds a property event to entities')
@@ -139,20 +154,26 @@ property.command('event')
   .option('-f, --file <file>', 'the entity files that should be modified', '**/*.json')
   .addOption(new Option('-p, --property <property name>', 'the name of the property to set').makeOptionMandatory(true))
   .option('-e, --event <event name>', 'a custom event name, otherwise defaults to "set_property_to_value')
-  .action(triggerEntityAddPropertyEvent);
+  .action(triggerEntityAddPropertyEvent)
+  .hook('postAction', printVersion);
 // #endregion
 
+// #region Package Commands
 let pkg = program.command('pkg')
   .description('Package manager for Bedrock files');
 
 pkg.command('list')
   .description('List packages at repo')
-  .action(triggerPackagesList);
+  .action(triggerPackagesList)
+  .hook('postAction', printVersion);
 
 pkg.command('import')
   .description('import packages from repo')
   .argument('<packages...>', 'the packages to import as either index value or name')
-  .action(triggerPackagesImport);
+  .action(triggerPackagesImport)
+  .hook('postAction', printVersion);
+
+// #endregion
 
 program.parse();
 
@@ -169,7 +190,7 @@ async function triggerCreateNewEntity(names: string[], options: OptionValues) {
   const type = options.type;
   const lang = options.lang;
   const client = options.client;
-  Entity.createNewEntity(names, lang, type, client);
+  await Entity.createNewEntity(names, lang, type, client);
 }
 
 async function triggerCreateNewItem(names: string[], options: OptionValues) {
@@ -178,7 +199,7 @@ async function triggerCreateNewItem(names: string[], options: OptionValues) {
   const edible = options.edible;
   const stack = options.stack;
   const attachable = options.attachable;
-  Item.createNewItem(names, lang, edible, stack, attachable);
+  await Item.createNewItem(names, lang, edible, stack, attachable);
 }
 
 async function triggerCreateNewBlock(names: string[], options: OptionValues) {
@@ -186,7 +207,7 @@ async function triggerCreateNewBlock(names: string[], options: OptionValues) {
   const lang = options.lang;
   const emissive = options.emissive;
   const table = options.table;
-  Block.createNewBlock(names, lang, emissive, table);
+  await Block.createNewBlock(names, lang, emissive, table);
 }
 
 async function triggerCreateNewAnimation(names: string[], options: OptionValues) {
@@ -194,7 +215,7 @@ async function triggerCreateNewAnimation(names: string[], options: OptionValues)
   const loop = options.loop;
   const commands = options.commands;
   const time = options.time;
-  Animation.createNewAnimation(names, loop, commands, time);
+  await Animation.createNewAnimation(names, loop, commands, time);
 }
 
 async function triggerCreateNewController(names:string[], options: OptionValues) {
@@ -204,7 +225,7 @@ async function triggerCreateNewController(names:string[], options: OptionValues)
   const anim = options.anim;
   const query = options.query;
   const transition = options.transition;
-  Animation.createNewController(names, entry, exit, anim, query, transition);
+  await Animation.createNewController(names, entry, exit, anim, query, transition);
 }
 
 async function triggerCreateNewFunction(names: string[], options: OptionValues) {
@@ -213,7 +234,7 @@ async function triggerCreateNewFunction(names: string[], options: OptionValues) 
   const number = options.number;
   const description = options.description;
   const source = options.source;
-  Function.createNewFunction(names, commands, number, description, source);
+  await Function.createNewFunction(names, commands, number, description, source);
 }
 
 async function triggerEntityAddAnim(names: string[], options: OptionValues) {
@@ -221,21 +242,21 @@ async function triggerEntityAddAnim(names: string[], options: OptionValues) {
   const family = options.type;
   const file = options.file;
   const script = options.script;
-  Entity.entityAddAnim(names, family, file, script);
+  await Entity.entityAddAnim(names, family, file, script);
 }
 
 async function triggerEntityAddGroup(group: string, options: OptionValues) {
   await setPaths();
   const family = options.type;
   const file = options.file;
-  Entity.entityAddGroup(group, family, file);
+  await Entity.entityAddGroup(group, family, file);
 }
 
 async function triggerEntityAddComponent(component: string, options: OptionValues) {
   await setPaths();
   const family = options.type;
   const file = options.file;
-  Entity.entityAddComponent(component, family, file);
+  await Entity.entityAddComponent(component, family, file);
 }
 
 async function triggerEntityAddProperty(names: string[], options: OptionValues) {
@@ -246,7 +267,7 @@ async function triggerEntityAddProperty(names: string[], options: OptionValues) 
   const values = options.values;
   const default_value = options.default;
   const client = options.client;
-  Entity.entityAddProperty(names, type, file, property, values, default_value, client);
+  await Entity.entityAddProperty(names, type, file, property, values, default_value, client);
 }
 
 async function triggerEntityAddPropertyEvent(names: string[], options: OptionValues) {
@@ -255,14 +276,14 @@ async function triggerEntityAddPropertyEvent(names: string[], options: OptionVal
   const file = options.file;
   const property = options.property;
   const event = options.event;
-  Entity.entityAddPropertyEvent(names, type, file, property, event);
+  await Entity.entityAddPropertyEvent(names, type, file, property, event);
 }
 
 async function triggerEntityAddDamageSensor(sensor: string, options: OptionValues) {
   await setPaths();
   const type = options.type;
   const file = options.file;
-  Entity.entityAddDamageSensor(sensor, type, file);
+  await Entity.entityAddDamageSensor(sensor, type, file);
 }
 
 async function triggerPackagesList() {
@@ -295,7 +316,15 @@ async function triggerPackagesImport(names: string[]) {
 async function triggerCreateVanillaEntity(names: string[], options: OptionValues) {
   await setPaths();
   const client = options.client;
-  Entity.createVanillaEntity(names, client);
+  await Entity.createVanillaEntity(names, client);
 }
 // #endregion
 
+async function printVersion() {
+  let result = await axios.get('https://registry.npmjs.org/bedrock-development-cli/latest');
+  let latest_version = result.data.version;
+  if (version !== latest_version) {
+    console.log(`${Global.chalk.yellow(`A new release of bed is available:`)} ${Global.chalk.cyan(`${version} → ${latest_version}`)}`);
+    console.log(`${Global.chalk.yellow('npm i bedrock-development-cli@latest -g')}`);
+  }
+}
