d187543ea8a4589fc31161924d5fffd81d1b808f
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2023-02-21 14:04:01 -0600

    v1.0.10

--- ts/index.ts
+++ ts/index.ts
@@ -10,7 +10,7 @@ import * as Package from 'bedrock-development/bin/app/package_manager';
 import axios from 'axios';
 
 let program = new Command();
-const version = '1.0.9'
+const version = '1.0.10'
 
 program
   .name('bed')
